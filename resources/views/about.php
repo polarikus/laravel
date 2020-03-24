5235d8486917e6b5f88e1223efc9a08d9877e6ae
Polarikus <igoran9607@gmail.com>
2020-03-25 00:12:40 +0300

    Initial commit

---
+++ resources/views/about.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <ul>
+        <li><a href="http://laravel.lc/main">Главная</a></li>
+        <li><a href="http://laravel.lc/about">О проекте</a></li>
+        <li><a href="http://laravel.lc/contacts">Контакты</a></li>
+        <li><a href="http://laravel.lc/news">Новости</a></li>
+    </ul>
+</header>
+<div class="content">
+    <p>Это проект на laravel</p>
+</div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/main', function () {
+    return view('main');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+Route::get('/news', function () {
+    return view('news');
+});
