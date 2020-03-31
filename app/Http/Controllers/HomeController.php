ade9ac8a23140955464ce8feb7f49c8fb2755d77
Polarikus <igoran9607@gmail.com>
2020-03-31 21:10:40 +0300

    Заметил, что версия laravel 7.3 - пришлось ставить бутстрап вручную(

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('main');
+        return view('main')->with('title', 'Главная');
     }
 
     public function contacts(){
